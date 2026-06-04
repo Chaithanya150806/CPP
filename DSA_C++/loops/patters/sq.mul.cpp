e0dc892ccb94ef1509aed3f3f1c84bd6ed59c558
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-06-04 17:18:54 +0530

    patters

--- DSA_C++/loops/patters/sq.mul.cpp
+++ DSA_C++/loops/patters/sq.mul.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main(){
     int  n = 3;
     char ch = 'A';
-    for (int i = 1 ; i <= n ; i++){
-        for (int j= 1;j<=n ; j++){
-            ch = ch+1;
+    for (int i = 0 ; i <= n ; i++){
+        for (int j= 0;j<=n ; j++){
+             ch+=1;
 
             cout << ch <<" ";
         }
