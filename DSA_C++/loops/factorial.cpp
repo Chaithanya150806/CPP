a5e50a5a1b698a977c4ed3b53d2aba4e4adfc592
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-06-04 08:31:50 +0530

    loop

---
+++ DSA_C++/loops/factorial.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n=5;
+    int sum=1;
+    for (int i = 1; i <=n ;i++){
+        sum = sum *i;
+    } 
+    cout << sum <<endl;
+    return 0;
+}
