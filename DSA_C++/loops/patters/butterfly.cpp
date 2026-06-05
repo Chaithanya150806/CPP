17a711e7757cc99ddd1dde11082792fe4ecb82da
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-06-05 19:07:51 +0530

    patters

---
+++ DSA_C++/loops/patters/butterfly.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n=4;
+    for (int i=0 ; i<n ;i++){
+        for (int j=0 ;j<i; j++){
+             cout <<"*";
+            
+        }
+      
+         for (int j=0;j<2*(n-i);j++){
+            cout <<" ";
+        }
+        for (int j=0 ;j<i ;j++){
+            cout <<"*";
+         }
+           
+        
+        cout <<endl;
+        
+    }
+
+    //lower
+    for (int i=n-1 ; i>1; i--){
+
+        for (int j=0; j<i ; j++){
+
+            cout <<"*";
+
+        }
+        for (int j=0 ;j<2*(n-i);j++){
+        cout <<" ";
+
+        }
+            for (int j=0; j<i ;j++){
+                cout <<"*";
+            }
+            
+        
+        cout <<endl;
+
+    }
+    
+    return 0;
+}
