c3de42690e35df3de348a8d8c6f5c34ee6074dd7
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-06-09 19:26:41 +0530

    array

--- DSA_C++/array/arr_ p1.cpp
+++ DSA_C++/array/arr_ p1.cpp
@@ -1,1 +1,14 @@
-#include <hh>
+#include <iostream>
+using namespace std;
+int main(){
+    int num[5];
+    int in=5;
+    for  (int i =0 ; i <in ;i++){
+        cin >> num[i];
+
+    }
+    for (int i=0 ;i<in ; i++){
+        cout << num[3];
+    }
+    return 0;
+}
