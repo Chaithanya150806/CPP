da74fe1e8922d288f6ccdf9db1b309759731df86
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-06-11 17:04:34 +0530

    array

--- DSA_C++/array/arr_intersion.cpp
+++ DSA_C++/array/arr_intersion.cpp
@@ -3,26 +3,21 @@ using namespace std;
 int main(){
     int arr1[]={1,2,3,4,5};
     int size1=5;
+
     int arr2[]={4,5,6,7,8};
     int size2=5;
+
     for (int i=0  ;i<size1;i++){
+
         for(int j=0 ;j<size2;j++){
+
             if (arr1[i]==arr2[j]){
                 cout <<arr1[i]<<" ";
                 
-
             }
            
-
-            
         }
         
-    }
-
-
-
-
-
-    
+    }   
     return 0;
 }
