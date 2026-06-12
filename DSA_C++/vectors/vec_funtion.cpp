219d086654712a7aa9ab3ba485b49c5186badc94
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-06-12 08:11:00 +0530

    vectors

--- DSA_C++/vectors/vec_funtion.cpp
+++ DSA_C++/vectors/vec_funtion.cpp
@@ -2,7 +2,24 @@
 #include <vector>
 using namespace std;
 int main(){
-    vector <int>
+    vector <int>vec={5,10,15,20,25};
+    cout <<vec.size()<<endl;
+    vec.push_back(30);
+    vec.push_back(35);
+    vec.push_back(40);
+    vec.push_back(45);
+    vec.pop_back();
+    vec.pop_back();
+   
+
+    for (int i :vec){
+        cout <<" "<<i<<endl;
+    }
+    cout<< vec.front()<<" ";
+     cout<< vec.at(3);
+
+
+
 
 
 
