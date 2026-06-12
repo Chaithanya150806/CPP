b704e64f79e5e8d0cc4b0449a59059a5beedfc34
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-06-12 07:55:36 +0530

    vec

---
+++ DSA_C++/vectors/vec_loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector <int> num={1,2,85,69,5};
+    for  (int i : num){
+        cout <<i <<endl;
+    }
+
+    return 0;
+}
