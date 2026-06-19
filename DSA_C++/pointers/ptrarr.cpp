afa23447b35e8393ff2af621cafcbedce6523022
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-06-19 12:27:02 +0530

    ptr

---
+++ DSA_C++/pointers/ptrarr.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[]={1,2,5,8};
+
+    cout << arr<<endl;
+    cout << *arr <<endl;
+    cout << *arr+1<<endl;
+    cout << *(arr+2)<<endl;
+    cout << *(arr+3);
+    return 0;
+
+}
