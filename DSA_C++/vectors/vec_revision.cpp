d148a8c2c1caf9018cb287c5540532de8419b5e6
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-07-03 15:11:59 +0530

    binary

---
+++ DSA_C++/vectors/vec_revision.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> revision(vector <int> & num,int n){
+    for (int i=0;i<=num.size();i++){
+        num.sort();
+
+    }
+}
+int main(){
+    vector <int> num = {5,2,8,1,9,2};
+    int n =6;
+   num.find(8)
+    return 0;
+
+    
+}
