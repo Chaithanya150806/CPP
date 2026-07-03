365ea2529609a720f67178f51542574db6b63c4b
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-07-03 15:41:39 +0530

    binary

--- DSA_C++/vectors/vec_revision.cpp
+++ DSA_C++/vectors/vec_revision.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
-vector<int> revision(vector <int> & num,int n){
-    for (int i=0;i<=num.size();i++){
-        num.sort();
-
-    }
-}
 int main(){
     vector <int> num = {5,2,8,1,9,2};
-    int n =6;
-   num.find(8)
+    int n =8;
+      sort(num.begin(),num.end());
+      cout << "sort :";
+      for (int val : num){
+        cout <<val <<" ";
+       }
+       cout <<endl;
+
+      auto it= find(num.begin(),num.end(),n);
+      if (it != num.end()){
+        cout << n <<" is in index "<<it -num.begin()<<endl;
+
+      }else {
+        cout <<n<< " not founded";
+      }
+       cout <<endl;
+    
+
+       cout <<"lagreast num ="<<*max_element(num.begin(),num.end())<<endl;
+  
     return 0;
 
     
