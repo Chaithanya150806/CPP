2022364215303802e2fc1460d5374f997d43e80c
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-07-08 08:18:37 +0530

    sort

--- DSA_C++/Sorting/bubble.cpp
+++ DSA_C++/Sorting/bubble.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
-}
+    int arr[]={5,3,2,1,4};
+    int n=5;
+    cout <<"Bubble sort array befor sorting :"<< arr <<endl;
+    for (int i=0;i<n-1;i++){
+        for (int j=0;j<n-i-1;j++){
+            if (arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    cout<<"Bubble sort :";
+    for (int i=0;i<n;i++){
+        cout <<arr[i] <<" ";
+    }
+    cout <<endl;
+    return 0;
+    }
