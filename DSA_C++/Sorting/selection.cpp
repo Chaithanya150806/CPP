890a5c762a3405d47aa665d8e6f9b1098e9b450c
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-07-08 14:15:21 +0530

    sort

--- DSA_C++/Sorting/selection.cpp
+++ DSA_C++/Sorting/selection.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
+    int arr[]={5,2,1,3,4};
+    int n=5;
+    cout <<"selection sort : ";
+    for (int i=0;i<n-1;i++){
+        int small=i;
+
+        for(int j=i+1;j<n;j++){
+
+            if  (arr[j]<arr[small]){
+                small=j;
+            }
+        }
+        swap(arr[i],arr[small]);
+    }
+    for(int i=0;i<n;i++){
+        cout <<arr[i];
+    }
 
 
 
     
-    return o;
+    return 0;
 }
