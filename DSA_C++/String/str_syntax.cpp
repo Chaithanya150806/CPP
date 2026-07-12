7555fe04332ef70fd568fc28a7e1d0ea9a708bb9
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-07-12 12:26:30 +0530

    string

---
+++ DSA_C++/String/str_syntax.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string name ="Chaithanya";
+    cout << "Name :"<<name <<endl;
+    string A = "apple" ;
+    string B = "banana" ;
+    string C = A+B;
+    cout << C << endl;
+    cout << A +" "+B<<endl;
+    string x="10";
+    string y="20";
+    string z=x+y;
+    cout <<"Ans will be 1020 '+' oper will work diff in str :" <<z <<endl;
+    return 0;
+}
