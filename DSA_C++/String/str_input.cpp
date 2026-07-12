39f5180b22f2caf958e700230e6ccc362287d7c1
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-07-12 14:17:27 +0530

    string

---
+++ DSA_C++/String/str_input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string Name ,age  ;
+    cout << "Enter your Name :";
+    cin >> Name ;
+    cout <<"Enter your age :";
+    cin>> age;
+
+
+    
+    return 0;
+}
