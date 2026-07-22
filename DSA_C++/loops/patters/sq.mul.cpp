3fc26e5a0b1f96bd3c637a48a14d0022a019bfef
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-07-22 09:35:36 +0530

    pat

--- DSA_C++/loops/patters/sq.mul.cpp
+++ DSA_C++/loops/patters/sq.mul.cpp
@@ -5,7 +5,7 @@ int main(){
     char ch = 'A';
     for (int i = 0 ; i <= n ; i++){
         for (int j= 0;j<=n ; j++){
-             ch+=1;
+            ch++;
 
             cout << ch <<" ";
         }
