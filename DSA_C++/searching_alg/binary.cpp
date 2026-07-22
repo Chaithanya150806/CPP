daba99104898fac99b6fde4ccacd720518dc987d
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-07-22 22:41:13 +0530

    pat

--- DSA_C++/searching_alg/binary.cpp
+++ DSA_C++/searching_alg/binary.cpp
@@ -4,19 +4,19 @@ using  namespace std;
 int main(){
     vector <int> arr ={1,2,3,4,5,6};
     int target = 5;
-    int low=0;
-    int high=arr.size()-1;
-    while(low <=high){
-        int mid=low+(high-low)/2;
+    int st=0;
+    int end=arr.size()-1;
+    while(st<=end){
+        int mid=st+(end-st)/2;
         if (arr[mid]==target){
            cout << "target =" <<mid<<endl;
            break;
         }
        else if (arr[mid] < target){
-           low =mid +1;
+           st =mid +1;
         }
          else if (arr[mid]>target){
-            high=mid -1;
+            end=mid -1;
         }    
     }
     return 0;
