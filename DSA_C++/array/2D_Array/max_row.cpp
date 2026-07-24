8fb276bd2aad30cd6e8e5698228d0aba945e80ce
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-07-24 17:45:49 +0530

    2d

--- DSA_C++/array/2D_Array/max_row.cpp
+++ DSA_C++/array/2D_Array/max_row.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <climits>
+#include <algorithm>
 using namespace std;
+    int maxrow(int mat[3][3],int row,int col){
+        int maxsum=INT_MIN;
+        for (int i=0;i<row;i++){
+            int rowsumi =0;
+            for (int j=0;j<col;j++){
+                rowsumi +=mat[i][j];
+            }
+            maxsum=max(maxsum,rowsumi);
+
+        }
+        return maxsum;
+    }
+
 int main(){
-    
+    int mat[3][3]={{2,5,1},{10,20,30},{100,20,2}};
+    cout <<maxrow(mat,3,3);
+    return 0;
 }
