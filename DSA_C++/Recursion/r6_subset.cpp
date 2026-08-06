8599247a5de1b7ac6e3fa8d60e3e429f148d4b29
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-08-07 00:01:59 +0530

    rev

--- DSA_C++/Recursion/r6_subset.cpp
+++ DSA_C++/Recursion/r6_subset.cpp
@@ -3,12 +3,20 @@
 using namespace std;
 void printsubset(vector<int> &arr,vector<int>&ans,int i){
     if(i == arr.size()){
-        for 
+        for (int val :ans){
+            cout <<val <<" ";
+        }
+        cout << endl;
+        return;
     }
+    ans.push_back(arr[i]);
+    printsubset(arr,ans,i+1);
+    ans.pop_back();
+    printsubset(arr,ans,i+1);
 }
-
-
 int main(){
-
+    vector <int> arr={1,2,3};
+    vector <int> ans;
+    printsubset(arr,ans,0);
     return 0;
 }
