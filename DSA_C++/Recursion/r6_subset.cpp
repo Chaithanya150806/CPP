d6ffac9c498240426e747c5574950b60cd656c5e
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-08-06 23:53:24 +0530

    rev

---
+++ DSA_C++/Recursion/r6_subset.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void printsubset(vector<int> &arr,vector<int>&ans,int i){
+    if(i == arr.size()){
+        for 
+    }
+}
+
+
+int main(){
+
+    return 0;
+}
--- DSA_C++/vectors/vec_funtion.cpp
+++ DSA_C++/vectors/vec_funtion.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 int main(){
     vector <int>vec={5,10,15,20,25};
+    vector <int>num={5,17,15,20,25};
     cout <<vec.size()<<endl;
     vec.push_back(30);
     vec.push_back(35);
@@ -10,6 +11,7 @@ int main(){
     vec.push_back(45);
     vec.pop_back();
     vec.pop_back();
+    num.pop_back();
    
 
     for (int i :vec){
