37a1ee4773745f95fac026c34eeb07e184130be5
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-08-29 15:38:32 +0530

    pattern

--- DSA_C++/DSA_Patterns/linear3.cpp
+++ DSA_C++/DSA_Patterns/linear3.cpp
@@ -1,9 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-
-
 int main() {
+    int arr[]={1,4,5,3,6};
+    int n=5;
+    int sum=0;
+    for (int i = 0 ; i < n ; i++){
+        sum = sum +arr[i];
+    }
+    cout << "sum =" <<sum;
 
     
     return 0;
